5dcf66b7022c6828b60bc3f9138de9c45fa357c3
冯振兴 <zhenxing@fanli.com>
2019-09-14 15:05:05 +0800

    18-SingleNumber[哈希，数字]

---
+++ 18-singleNumber.py
@@ -0,0 +1,26 @@
+class Solution(object):
+    def singleNumber1(self, nums):
+        nums.sort();
+        x = 0;
+        while x < len(nums):
+        	if x == len(nums)-1:
+        		return nums[x];
+        	elif nums[x] != nums[x+1]:
+        		return nums[x];
+        	else:
+        		x = x+2;
+    def singleNumber2(self, nums):
+        map = {};
+        for x in nums:
+        	if map.has_key(x) == False:
+        		map[x] = 0;
+        	else:
+        		map.pop(x);
+        return map.keys()[0];
+    def singleNumber3(self, nums):
+        result = 0;
+        for x in nums:
+        	result = result ^ x;
+        return result;
+    def singleNumber(self, nums):
+        return sum(set(nums))*2 - sum(nums);
